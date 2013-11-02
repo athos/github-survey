6613d46d9af7de9122e931958b62f8763e6ced36
OHTA Shogo <athos0220@gmail.com>
2013-11-02 12:39:00 +0900

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to github-survey
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/github_survey/core_test.clj
@@ -0,0 +1,7 @@
+(ns github-survey.core-test
+  (:require [clojure.test :refer :all]
+            [github-survey.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
